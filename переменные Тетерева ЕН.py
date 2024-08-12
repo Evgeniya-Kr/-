83c0b99e58a18c47140787bfa7bf8f13aa67e767
Evgeniya-Kr <jenya-kr@yandex.ru>
2024-08-12 21:18:59 +0700

    Add files via upload

---
+++ переменные Тетерева ЕН.py
@@ -0,0 +1,7 @@
+name = "Курс: Python,"
+first_name = "всего задач: 12,"
+secong_name = "затрачено часов: 1,5,"
+third_name = "среднее время выполнения "
+print(name, first_name, secong_name, third_name, (1.5/12), 'часа')
+
+
