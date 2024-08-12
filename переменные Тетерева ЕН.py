b3e37322f800ad003c7c77ea044133fd480c6fff
Evgeniya-Kr <jenya-kr@yandex.ru>
2024-08-13 03:51:33 +0700

    Add files via upload

--- переменные Тетерева ЕН.py
+++ переменные Тетерева ЕН.py
@@ -4,6 +4,6 @@ number_of_tasks = 12
 secong_name = "затрачено часов: "
 number_of_hours = 1.5
 third_name = "среднее время выполнения "
-print(name, first_name, number_of_tasks, secong_name, number_of_hours, third_name, ((1.5)/(12)), 'часа')
+print(name, first_name, number_of_tasks, secong_name, number_of_hours, third_name, ((number_of_hours)/(number_of_tasks)), 'часа')
 
 
